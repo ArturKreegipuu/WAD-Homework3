94acf551058563f588227419d6f5536c864da436
ArturKreegipuu <artur.kreegipuu@ut.ee>
2022-11-18 15:12:20 +0200

    dorpdown goes away on click

--- wad-homework3/src/components/Header.vue
+++ wad-homework3/src/components/Header.vue
@@ -29,6 +29,21 @@ export default {
       console.log("Button pressed")
     },
 
+  },
+  mounted() {
+    window.onclick = function (event) {
+      if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content")
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+          let openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains("show")) {
+            openDropdown.classList.remove("show");
+          }
+
+        }
+      }
+    }
   }
 
 }
